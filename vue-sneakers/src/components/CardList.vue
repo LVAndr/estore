31c8e831614ffe68ebffc12dcb6c1dccef1b1272
LVAndr <vadim.lylko@gmail.com>
2023-12-25 01:24:21 +0200

    Add Card, CardList

---
+++ vue-sneakers/src/components/CardList.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import Card from "./Card.vue";
+
+
+const onClick = ()=>{
+  alert(222)
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card title="Мужские Кроссовки Nike Blazer Mid Suede" :price="2300" image-url="/sneakers/sneakers-1.jpg"
+    :is-added="true" :is-favorite="true"
+    :on-click-add="onClick"
+    />
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
