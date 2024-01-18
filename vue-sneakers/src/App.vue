a2dbce89da1ceee54da1198ad62e7689a0f5f891
LVAndr <vadim.lylko@gmail.com>
2024-01-18 03:34:37 +0200

    refactoring, add vue-router

--- vue-sneakers/src/App.vue
+++ vue-sneakers/src/App.vue
@@ -1,13 +1,11 @@
 <script setup>
-import {onMounted, ref, reactive, watch, provide, computed} from "vue";
-import axios from "axios";
+import {ref, watch, provide, computed} from "vue";
+
 
 import Header from "@/components/Header.vue";
-import CardList from "@/components/CardList.vue";
 import Drawer from "@/components/Drawer.vue";
 
-
-const items = ref([]);
+/*Cart (START)*/
 const cart = ref([]);
 const isCreatingOrder = ref(false);
 
@@ -32,10 +30,7 @@ const cartIsEmpty = computed(()=> cart.value.length === 0);
 
 const cartButtonDisabled = computed(()=> isCreatingOrder.value || cartIsEmpty.value);
 
-const filters = reactive({
-  sortBy:'title',
-  searchQuery: '',
-})
+
 const addToCart = (item) => {
   cart.value.push(item);
   item.isAdded = true;
@@ -64,120 +59,10 @@ const removeFromCart = (item) => {
   cart.value.splice(cart.value.indexOf(item), 1)
   item.isAdded = false;
 }
-const onClickAddPlus = (item) => {
-  if (!item.isAdded){
-    addToCart(item);
-  }else{
-    removeFromCart(item);
-  }
-}
-
-const onChangeSelect = (event) =>{
-  filters.sortBy = event.target.value
-}
-const onChangeSearchInput = (event) => {
-  filters.searchQuery = event.target.value;
-}
-
-const fetchFavorites = async ()=>{
-  try{
-
-    const {data: favorites} = await axios.get(`https://0f19d9182f91cfff.mokky.dev/favorites`);
-
-    items.value = items.value.map(item => {
-      const favorite = favorites.find(favorite => favorite.parentId === item.id);
-
-      if (!favorite){
-        return item
-      }
-      return {
-        ...item,
-        isFavorite: true,
-        favoriteId: favorite.id,
-      }
-    });
-  }
-
-  catch (err){
-    console.log(err)
-  }
-}
-
-const addToFavorite = async (item)=>{
-  try {
-    if (!item.isFavorite){
-      const obj = {
-        parentId: item.id
-      }
-
-      item.isFavorite = true;
-
-      const {data} = await axios.post(`https://0f19d9182f91cfff.mokky.dev/favorites`, obj);
-
-      item.favoriteId = data.id;
-      console.log(data)
-    }else {
-      item.isFavorite = false;
-
-      await axios.delete(`https://0f19d9182f91cfff.mokky.dev/favorites/${item.favoriteId}`);
-      item.favoriteId = null;
-    }
-
-  }catch (err){
-    console.log(err)
-  }
-}
-const fetchItems = async ()=>{
-  try{
-    const params = {
-      sortBy: filters.sortBy,
-     // searchQuery: filters.searchQuery
-    }
-    if (filters.searchQuery){
-      params.title = `*${filters.searchQuery}*`
-    }
-    const {data} = await axios.get(
-        `https://0f19d9182f91cfff.mokky.dev/items`,
-        {params}
-    );
-
-    items.value = data.map((obj)=>({
-      ...obj,
-      isFavorite: false,
-      favoriteId: null,
-      isAdded: false
-    }));
-  }
-  catch (err){
-    console.log(err)
-  }
-}
-onMounted(async ()=>{
-  const localCart = localStorage.getItem('cart');
-  cart.value = localCart ? JSON.parse(localCart) : [];
-
-  await fetchItems();
-  await fetchFavorites();
-
-  items.value = items.value.map((item)=> ({
-    ...item,
-    isAdded: cart.value.some((cartItem) => cartItem.id === item.id)
-  }));
-});
-
-watch(filters, fetchItems);
-
-watch(cart, ()=> {
-  items.value = items.value.map((item)=> ({
-    ...item,
-    isAdded: false
-  }))
-}
-);
 
 watch(cart, ()=>{
-  localStorage.setItem('cart', JSON.stringify(cart.value))
-},
+      localStorage.setItem('cart', JSON.stringify(cart.value))
+    },
     {deep: true}
 );
 
@@ -188,6 +73,11 @@ provide('cart', {
   addToCart,
   removeFromCart
 });
+
+/*Cart (END)*/
+
+
+// watch(filters, fetchItems);
 
 </script>
 
@@ -203,32 +93,7 @@ provide('cart', {
     <Header :total-price="totalPrice" @open-drawer="openDrawer"/>
 
     <div class="p-10">
-      <div class="flex justify-between items-center">
-        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
-
-
-        <div class="flex gap-4">
-          <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
-            <option value="name">По названию</option>
-            <option value="price">По цене (дешевые)</option>
-            <option value="-price">По цене (дорогие)</option>
-            <option></option>
-            <option></option>
-          </select>
-
-          <div class="relative">
-            <img class="absolute left-4 top-3" src="/search.svg" alt="Search">
-            <input
-                @input="onChangeSearchInput"
-                class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400" type="text" placeholder="Поиск">
-          </div>
-        </div>
-
-      </div>
-
-      <div class="mt-10">
-        <CardList :items="items" @add-to-favorite="addToFavorite" @add-to-cart="onClickAddPlus" />
-      </div>
+      <router-view></router-view>
     </div>
 
   </div>
---
+++ vue-sneakers/src/pages/Favorites.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<p>My favorites</p>
+</template>
+
+<style scoped>
+
+</style>
